4be3ba0099466761d223e6b9531889a41761d49c
kerasyadekk <azizazudinmaharani@gmail.com>
2025-02-17 18:11:47 +0700

    Update jsbuttung.js

--- jsbuttung.js
+++ jsbuttung.js
@@ -2,8 +2,7 @@
         $('<a title="MASUK MELALUI TELEGRAM" href="https://t.me/lotto03app_bot" target="_blank" rel="nofollow" class="buttonWrap buttong button-blue contactSubmitButton">MASUK MELALUI TELEGRAM</a>').insertAfter(".button-green");
 
     },false);
-</script>
-<script>
+
     document.addEventListener("DOMContentLoaded",function(){
         $('<a title="vpn warp 1.1.1.1" href="https://play.google.com/store/search?q=warp&c=apps&hl=en_id" target="_blank" rel="nofollow" class="buttonWrap buttong button-blue contactSubmitButton">VPN.ANTI.BLOKIR</a>').insertAfter(".button-green");
 
